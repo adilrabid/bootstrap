a89890245c18bb75ed10e6324f00a1908525589a
adilrabid <rabid1819@gmail.com>
2025-07-30 23:11:24 +0600

    first build for dx

--- build/postcss.config.mjs
+++ build/postcss.config.mjs
@@ -8,6 +8,17 @@ export default context => {
   return {
     map: context.file.dirname.includes('examples') ? false : mapConfig,
     plugins: {
+      'postcss-prefix-selector' : {
+        prefix: 'dx-',
+        transform: function (prefix, selector) {
+          if (selector.startsWith('.')) {
+            return `.${prefix}${selector.slice(1)}`; // Add 'dx-' to classes
+          } else if (selector.startsWith('#')) {
+            return `#${prefix}${selector.slice(1)}`; // Add 'dx-' to IDs
+          }
+          return selector; // Leave other selectors unchanged
+        }
+      },
       autoprefixer: {
         cascade: false
       },
